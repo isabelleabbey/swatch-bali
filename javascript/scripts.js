b81567eb6bc8d4482705beac7d737447a66d2a84
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2024-06-03 16:08:21 +0200

    copied template over

---
+++ javascript/scripts.js
@@ -0,0 +1,4 @@
+// Auto update footer year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+// If using a different copyright for mobile, ensure to include the below. ID will only work once
+// document.getElementById("year-mob").innerHTML = new Date().getFullYear();
