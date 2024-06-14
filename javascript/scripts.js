3c4a3184cc2b9ef41c548d6d62376753b63cf991
isabelleabbey <iabbeyvital@gmail.com>
2024-06-14 10:28:11 +0100

    new mobile tweaks

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,36 @@
-// Auto update footer year
-document.getElementById("year").innerHTML = new Date().getFullYear();
-// If using a different copyright for mobile, ensure to include the below. ID will only work once
-// document.getElementById("year-mob").innerHTML = new Date().getFullYear();
+
+
+
+
+
+// image slideshow slick
+  
+  
+  $('.slider-container').slick({
+    autoplay: true, // Do we want it to autoplay? true or false
+    autoplaySpeed: 1000, // How long between each slide when auto-playing
+    speed: 500, // How fast is the transition in milliseconds
+    arrows: false, // Do you want to show arrows to trigger each slide
+    accessibility: true, // Enables keyboard tabbing and arrow key navigation
+    dots: false, // Enables the dots below to show how many slides
+    fade: true, // Changes the animate from slide to fade if true
+    infinite: true, // When true, means that it will scroll in a circle
+    pauseOnHover: false, // When true means the autoplay pauses when hovering
+    pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+  });
+
+
+  
+  $('.continuous').slick({
+    speed: 9000, 
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false,
+    dots: false,
+  });  
+
+    
