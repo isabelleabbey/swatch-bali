cc7b0de747af3d865b7cf3aea6c89eda0adf5571
Carly <cmc0294@gmail.com>
2024-07-29 12:43:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './_includes/**/*.{html,js,md}',
     './_layouts/**/*.{html,js,md}',
+    './/**/*.{html,md}',
     './_*/*.{html,js,md}',
     './*.{html,js,md}'
   ],
